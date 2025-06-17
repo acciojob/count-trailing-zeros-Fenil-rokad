fb473aa730c4ef1878096bb756ac2032c5b5975f
Fenil-rokad <fenilrokad3@gmail.com>
2025-06-17 13:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function trailingZeros(n) {
-  let count = 0;
-  for (let i = 5; n / i >= 1; i *= 5) {
-    count += Math.floor(n / i);
-  }
-  return count;
-}
+      let count = 0;
+      for (let i = 5; Math.floor(n / i) >= 1; i *= 5) {
+        count += Math.floor(n / i);
+      }
+      return count;
+    }
 
-const input = prompt("Enter a non-negative integer:");
-const num = parseInt(input);
+    const input = prompt("Enter a non-negative integer:");
+    const num = parseInt(input);
 
-if (!isNaN(num) && num >= 0) {
-  alert("Trailing zeros in " + num + "! is: " + trailingZeros(num));
-} else {
-  alert("Please enter a valid non-negative integer.");
-}
+    if (!isNaN(num) && num >= 0) {
+      const result = trailingZeros(num);
+      alert("Trailing zeros in " + num + "! is: " + result);
+    } else {
+      alert("Please enter a valid non-negative integer.");
+    }
